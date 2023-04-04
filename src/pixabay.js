684efd7fee4ac2ae8bedf2bc1b08db92730acfcd
roman10rb <roma10rb@gmail.com>
2023-04-04 20:54:10 +0300

    add async

--- src/pixabay.js
+++ src/pixabay.js
@@ -4,14 +4,19 @@ export class PixabayApi {
     q = null;
     page = 0;
     per_page = 40;
-   
+    
     async fetchPhotos() {
-
-        const response = await fetch(`${this.#BASE_URL}?key=${this.#API_KEY}&q=${this.q}&image_type=photo&page=${this.page}&per_page=${this.per_page}&orientation=horizontal&safesearch=true`);
-
-       
-        return await response.json();
+        try {
+            const response = await fetch(`${this.#BASE_URL}?key=${this.#API_KEY}&q=${this.q}&image_type=photo&page=${this.page}&per_page=${this.per_page}&orientation=horizontal&safesearch=true`);
+        
+        if (!response.ok) {
+                throw new Error(response.status);
+            }
+            return response.json();
+        } catch (e) {
+            console.log(e.status)
         }
+           
     }
   
-
+}
